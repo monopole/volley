3440b3f98964a1342025be7fa0f834be4c9edf74
ubuntu x01 <jregan@google.com>
2015-08-18 22:29:45 -0700

    moar balls

--- screen/screen.go
+++ screen/screen.go
@@ -6,24 +6,21 @@ package screen
 import (
 	"github.com/monopole/croupier/model"
 	"github.com/monopole/croupier/sprite"
-	"golang.org/x/mobile/exp/gl/glutil"
 	"golang.org/x/mobile/gl"
-	"log"
+)
+
+const (
+	opaque = 1.0
+	red    = 0.1
+	green  = 0.1
+	blue   = 0.1
 )
 
 type Screen struct {
-	program  gl.Program
-	position gl.Attrib
-	offset   gl.Uniform
-	color    gl.Uniform
-	zball    *sprite.Ball
-	opaque   float32
-	green    float32
-	red      float32
-	blue     float32
-	gray     float32
-	width    float32
-	height   float32
+	zball  []*sprite.Ball
+	gray   float32
+	width  float32
+	height float32
 }
 
 func NewScreen() *Screen {
@@ -31,22 +28,9 @@ func NewScreen() *Screen {
 }
 
 func (s *Screen) Start() {
-	var err error
-	s.opaque = 1.0
-	s.red = 0.1
-	s.green = 0.1
-	s.blue = 0.1
 
-	s.program, err = glutil.CreateProgram(sprite.VertexShader, sprite.FragmentShader)
-	if err != nil {
-		log.Printf("error creating GL program: %v", err)
-		return
-	}
-
-	s.zball = sprite.NewBall()
-	s.position = gl.GetAttribLocation(s.program, "position")
-	s.color = gl.GetUniformLocation(s.program, "color")
-	s.offset = gl.GetUniformLocation(s.program, "offset")
+	s.zball = []*sprite.Ball{sprite.NewBall()}
+	//	s.zball = []*sprite.Ball{sprite.NewBall(), sprite.NewBall()}
 }
 
 func (s *Screen) ReSize(width float32, height float32) {
@@ -63,31 +47,27 @@ func (s *Screen) Height() float32 {
 }
 
 func (s *Screen) Paint(balls []*model.Ball) {
-	gl.ClearColor(s.red, s.green, s.blue, s.opaque)
+	gl.ClearColor(red, green, blue, opaque)
 	gl.Clear(gl.COLOR_BUFFER_BIT)
-
-	gl.UseProgram(s.program)
 
 	s.gray += 0.01
 	if s.gray > 1 {
 		s.gray = 0
 	}
-	gl.Uniform4f(s.color, s.gray, 0, s.gray, s.opaque)
 
 	if len(balls) > 0 {
 		b := balls[0]
-		gl.Uniform2f(s.offset, b.GetPos().X/s.width, b.GetPos().Y/s.height)
+		x := b.GetPos().X / s.width
+		y := b.GetPos().Y / s.height
+		// Experimenting with multiple balls not working.
+		s.zball[0].Draw(s.gray, x, y)
+		//		s.zball[1].Draw(s.gray, x+100, y+10)
 	}
-	gl.EnableVertexAttribArray(s.position)
-	gl.VertexAttribPointer(s.position, sprite.CoordsPerVertex, gl.FLOAT, false, 0, 0)
-	gl.DrawArrays(gl.TRIANGLES, 0, sprite.VertexCount)
-
-	gl.DisableVertexAttribArray(s.position)
 
 	// debug.DrawFPS(c)
 }
 
 func (s *Screen) Stop() {
-	gl.DeleteProgram(s.program)
-	s.zball.DeleteBuffer()
+	s.zball[0].Delete()
+	//	s.zball[1].Delete()
 }
--- sprite/ball.go
+++ sprite/ball.go
@@ -3,28 +3,31 @@ package sprite
 import (
 	"encoding/binary"
 	"golang.org/x/mobile/exp/f32"
+	"golang.org/x/mobile/exp/gl/glutil"
 	"golang.org/x/mobile/gl"
+	"log"
 )
 
 const (
-	CoordsPerVertex = 3
-	VertexCount     = 4
+	coordsPerVertex = 3
+	vertexCount     = 4
+	opaque          = 1 // 0 is transparent
 
-	VertexShader = `#version 100
-uniform vec2 offset;
-attribute vec4 position;
+	vertexShader = `#version 100
+uniform vec2 jrOffset;
+attribute vec4 jrPosition;
 void main() {
 	// offset comes in with x/y values between 0 and 1.
 	// position bounds are -1 to 1.
-	vec4 offset4 = vec4(2.0*offset.x-1.0, 1.0-2.0*offset.y, 0, 0);
-	gl_Position = position + offset4;
+	vec4 offset4 = vec4(2.0*jrOffset.x-1.0, 1.0-2.0*jrOffset.y, 0, 0);
+	gl_Position = jrPosition + offset4;
 }`
 
-	FragmentShader = `#version 100
+	fragmentShader = `#version 100
 precision mediump float;
-uniform vec4 color;
+uniform vec4 jrColor;
 void main() {
-	gl_FragColor = color;
+	gl_FragColor = jrColor;
 }`
 )
 
@@ -37,7 +40,11 @@ var triangleData = f32.Bytes(binary.LittleEndian,
 )
 
 type Ball struct {
-	buf gl.Buffer
+	buf      gl.Buffer
+	program  gl.Program
+	position gl.Attrib
+	offset   gl.Uniform
+	color    gl.Uniform
 }
 
 func NewBall() *Ball {
@@ -45,13 +52,31 @@ func NewBall() *Ball {
 	b.buf = gl.CreateBuffer()
 	gl.BindBuffer(gl.ARRAY_BUFFER, b.buf)
 	gl.BufferData(gl.ARRAY_BUFFER, triangleData, gl.STATIC_DRAW)
+	var err error
+	b.program, err = glutil.CreateProgram(vertexShader, fragmentShader)
+	if err != nil {
+		log.Printf("error creating GL program: %v", err)
+		return nil
+	}
+	b.position = gl.GetAttribLocation(b.program, "jrPosition")
+	b.color = gl.GetUniformLocation(b.program, "jrColor")
+	b.offset = gl.GetUniformLocation(b.program, "jrOffset")
 	return b
 }
 
-func (b *Ball) Buffer() gl.Buffer {
-	return b.buf
+func (b *Ball) Draw(myColor float32, nx float32, ny float32) {
+	gl.UseProgram(b.program)
+
+	gl.Uniform4f(b.color, myColor, 0, myColor, opaque)
+	gl.Uniform2f(b.offset, nx, ny)
+
+	gl.EnableVertexAttribArray(b.position)
+	gl.VertexAttribPointer(b.position, coordsPerVertex, gl.FLOAT, false, 0, 0)
+	gl.DrawArrays(gl.TRIANGLES, 0, vertexCount)
+	gl.DisableVertexAttribArray(b.position)
 }
 
-func (b *Ball) DeleteBuffer() {
+func (b *Ball) Delete() {
+	gl.DeleteProgram(b.program)
 	gl.DeleteBuffer(b.buf)
 }
