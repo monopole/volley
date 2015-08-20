ac5e54f3d2bef60519ba92efa2b552f4103929f8
jregan <jregan@google.com>
2015-08-20 06:21:58 -0700

    moarballs

--- sprite/ball.go
+++ sprite/ball.go
@@ -2,51 +2,58 @@ package sprite
 
 import (
 	"encoding/binary"
+	"github.com/monopole/croupier/model"
 	"golang.org/x/mobile/exp/f32"
 	"golang.org/x/mobile/exp/gl/glutil"
 	"golang.org/x/mobile/gl"
 	"log"
 	"math"
+	"math/rand"
 )
 
 const (
+	extraBalls      = 10
 	coordsPerVertex = 3
 	vertexCount     = 4
-	opaque          = 1     // 0 is transparent
-	anotherBall     = false // testing
+	opaque          = 1    // 0 is transparent
+	anotherBall     = true // testing
 
 	/*
-				Coordinates of touch events, ball locations,
-				impulses, etc.
 
-				  ( 0, 0) ...  ( W, 0)
-				  ...             ...
-				  ( 0, H) ...  ( W, H)
+		Coordinates of touch events, ball locations,
+		impulses, etc. are in screen coords
 
-				Where W and H are the pixel width and height
-				of the screen.
+		  ( 0, 0) ...  ( W, 0)
+		  ...             ...
+		  ( 0, H) ...  ( W, H)
 
-				The {x,y} passed to Draw below is a strictly
-				positive normalized screen coordinate, with
-				width and height divided out:
+		where W and H are the pixel width and height
+		of the window.
 
-				  ( 0, 0) ...  ( 1, 0)
-				  ...             ...
-				  ( 0, 1) ...  ( 1, 1)
+		The {x,y} passed to Draw below is assumed to
+		be a non-negative normalized screen coordinate, with
+		width and height divided out:
 
-				This must be converted to OpenGL coords:
+		  ( 0, 0) ...  ( 1, 0)
+		  ...             ...
+		  ( 0, 1) ...  ( 1, 1)
 
-				  (-1, 1) ...  ( 1, 1)
-				  ...    (0, 0)    ...
-				  (-1,-1) ...  ( 1,-1)
+		This must be converted to OpenGL coords:
 
-				This is both a doubling and a sign flip
-				for the Y axis.  Transform is:
+		  (-1, 1) ...  ( 1, 1)
+		  ...    (0, 0)    ...
+		  (-1,-1) ...  ( 1,-1)
 
-				      X =  2 * ( oldX - 0.5 ) = 2 oldX - 1
-				      Y = -2 * ( oldY - 0.5 ) = 1 - 2 oldY
+		This is both a doubling and a sign flip
+		for the Y axis.  The transform is:
 
-		    This transform is done by the shader below.
+		  X =  2 * ( oldX - 0.5 ) = 2 oldX - 1
+		  Y = -2 * ( oldY - 0.5 ) = 1 - 2 oldY
+
+		and is performed by the shader below.  It could
+		be done in Go on the CPU, but might as well let
+		the GPU contribute.
+
 	*/
 	vertexShader = `#version 100
 uniform vec2 jrOffset;
@@ -65,20 +72,21 @@ void main() {
 )
 
 type Ball struct {
-	buf      gl.Buffer
-	program  gl.Program
-	position gl.Attrib
-	offset   gl.Uniform
-	color    gl.Uniform
+	buf       gl.Buffer
+	program   gl.Program
+	position  gl.Attrib
+	offset    gl.Uniform
+	color     gl.Uniform
+	moarBalls []model.Vec
 }
 
 var triangleData []byte
 
-// Characteristic vertex values of an equilateral triangle in opengl
-// window coords.  The base of such a window is two 'units' wide
-// (-1..1), so a triangle with side == 2 just fits (it's 'huge').
+// Characteristic values of an equilateral triangle in opengl window
+// coords.  The base of such a window is two 'units' wide (-1..1), so
+// a triangle with side == 2 just fits inside a window.
 func computeTriangleLengths() (float32, float32) {
-	side := 2.0 / 8.0 // A fraction of the characteristic distance.
+	side := 2.0 / 8.0 // Take a fraction of two, the characteristic size.
 	halfBase := side / 2.0
 	halfHeight := math.Sqrt(side*side-halfBase*halfBase) / 2.0
 	return float32(halfBase), float32(halfHeight)
@@ -94,7 +102,14 @@ func makeTriangleData() []byte {
 }
 
 func NewBall() *Ball {
+
 	b := &Ball{}
+
+	b.moarBalls = make([]model.Vec, extraBalls)
+	for i := 0; i < extraBalls; i++ {
+		b.moarBalls[i] = model.Vec{rand.Float32(), rand.Float32()}
+	}
+
 	b.buf = gl.CreateBuffer()
 	gl.BindBuffer(gl.ARRAY_BUFFER, b.buf)
 
@@ -124,17 +139,16 @@ func (b *Ball) Draw(myColor float32, nx float32, ny float32) {
 	gl.EnableVertexAttribArray(b.position)
 	gl.VertexAttribPointer(b.position, coordsPerVertex, gl.FLOAT, false, 0, 0)
 	gl.DrawArrays(gl.TRIANGLES, 0, vertexCount)
+
+	gl.Uniform4f(b.color, 0, 1.0, 1.0, opaque)
+	if len(b.moarBalls) > 0 {
+		for _, v := range b.moarBalls {
+			gl.Uniform2f(b.offset, v.X, v.Y)
+			gl.DrawArrays(gl.TRIANGLES, 0, vertexCount)
+		}
+	}
 	gl.DisableVertexAttribArray(b.position)
 
-	if anotherBall {
-		gl.Uniform4f(b.color, 0, 0, myColor, opaque)
-		gl.Uniform2f(b.offset, nx+.2, ny+.2)
-
-		gl.EnableVertexAttribArray(b.position)
-		gl.VertexAttribPointer(b.position, coordsPerVertex, gl.FLOAT, false, 0, 0)
-		gl.DrawArrays(gl.TRIANGLES, 0, vertexCount)
-		gl.DisableVertexAttribArray(b.position)
-	}
 }
 
 func (b *Ball) Delete() {
