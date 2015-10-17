fe52fc48104e552fa8c97a44e7da9f90914293eb
Asim Shankar <asimshankar@gmail.com>
2015-10-16 20:34:49 -0700

    Include code generated from VDL.
    One step closer towards quick setup - no need to worry about installing
    this other tool (vdl) and running a command before being able to try out
    volley.

---
+++ ifc/doc.go
@@ -0,0 +1,8 @@
+// Package ifc defines the interfaces between networked participants of the
+// game.
+//
+// This package is generated from VDL. To regenerate:
+//
+//   go get v.io/x/ref/cmd/vdl
+//   VDLPATH=$GOPATH/src $GOPATH/bin/vdl generate github.com/monopole/volley/ifc
+package ifc
